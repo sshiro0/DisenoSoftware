60767b6ed2fa8d0ced2d3a712ceba225e3c624ec
Pvilla14 <pablo.villagran.hermanns@gmail.com>
2025-06-11 15:40:07 -0400

    me falto esto

--- manejo_paquetes/views.py
+++ manejo_paquetes/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import *
+from .utility.direcciones import map_route
 
 # Create your views here.
 
@@ -20,4 +21,18 @@ def Ver_Paquetes_Entrega(request):
     return render(request, 'Paquetes_entrega.html', {
         'entregas' : entregas,
         'paquetes' : paquetes,
+    })
+
+def Ver_ruta(request):
+    entregas = Entrega.objects.all()
+    paquetes = Paquete.objects.all()
+    nombres_mapas = []
+    for entrega in entregas: 
+        paquetes_direccion = Paquete.objects.filter(Direccion = entrega.Destino)
+        coordenadas = [tuple(map(float, paquete.Destino.split(','))) for paquete in paquetes_direccion]
+        nombremapa = f"mapa_entrega_{entrega.id}" 
+        nombres_mapas.append(f"{nombremapa}.html")
+        map_route(dest_coords=list(reversed(coordenadas)), sede_coords=Bodegas_cords, nombre_mapa=nombremapa)
+    return render(request, 'Rutas_entregas.html',{
+        'mapas' : nombres_mapas,
     })
