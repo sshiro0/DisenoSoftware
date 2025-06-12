0eac533079ff9a0f77539936ac41855b62f8b36e
Pvilla14 <pablo.villagran.hermanns@gmail.com>
2025-06-11 23:15:32 -0400

    Se creo el login y  se conectaron paginas

--- manejo_paquetes/urls.py
+++ manejo_paquetes/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path('', views.hello),
-    path('Paquetes/<int:id>', views.Ver_Paquetes_Cliente),
-    path('Paquetes_entrega/', views.Ver_Paquetes_Entrega),
-    path('Mapas/', views.Ver_ruta),
+    path('', views.login_user),
+    path('Paquetes/<int:id>/', views.Ver_Paquetes_Cliente, name='ver_paquetes_cliente'),
+    path('Paquetes_entrega/', views.Ver_Paquetes_Entrega, name='ver_entregas'),
+    path('Mapas/', views.Ver_ruta, name='ver_ruta'),
 ]
--- manejo_paquetes/views.py
+++ manejo_paquetes/views.py
@@ -1,12 +1,36 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from django.contrib import messages
 from .models import *
 from .utility.direcciones import map_route
 
 # Create your views here.
 
-def hello(request):
-    return HttpResponse("<h1>Hello World</h1>") 
+def login_user(request):
+    if request.method == 'POST':
+        correo = request.POST['correo']
+        contrasena = request.POST['contrasena']
+        try:
+            usuario = Usuario.objects.filter(Correo=correo, Contrasena=contrasena).first()
+        
+            if usuario is None:
+                return render(request, 'login.html')
+
+            if Cliente.objects.filter(ID_cliente__ID_usuario=usuario.ID_usuario).exists():
+                return redirect('ver_paquetes_cliente', id=usuario.ID_usuario)
+
+            if Conductor.objects.filter(ID_conductor__ID_usuario=usuario.ID_usuario).exists():
+                return redirect('ver_entregas')
+
+            if Administrador.objects.filter(ID_admin__ID_usuario=usuario.ID_usuario).exists():
+                return redirect('/admin/')  # o tu vista personalizada: redirect('panel_admin')
+
+            else:
+                messages.error(request, 'Tu cuenta no está asignada como cliente, conductor ni administrador.')
+
+        except Usuario.DoesNotExist:
+            messages.error(request, 'Correo o contraseña incorrectos.')
+
+    return render(request, 'login.html')
 
 def Ver_Paquetes_Cliente(request, id):
     Paquetes = Paquete.objects.all()
