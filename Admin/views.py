a2e3433f874ca60e591270b88452ee9c5e41b654
pcfuenr <panchocifuentes1234@gmail.com>
2025-07-09 00:27:22 -0400

    primer commit con lo minimo de todo

--- Admin/views.py
+++ Admin/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
 # Create your views here.
+def create_paquete(request):
+    if request.method == 'POST':
+        pass
+    return render(request, 'Admin_Paquete.html')
+
+def create_Conductor(request):
+    if request.method == 'POST':
+        pass
+    return render(request, 'Admin_CrearConductor.html')
--- manejo_paquetes/urls.py
+++ manejo_paquetes/urls.py
@@ -5,4 +5,8 @@ urlpatterns = [
     path('Paquetes/<int:id>/', views.Ver_Paquetes_Cliente, name='ver_paquetes_cliente'),
     path('Paquetes_entrega/', views.Ver_Paquetes_Entrega, name='ver_entregas'),
     path('Mapas/', views.Ver_ruta, name='ver_ruta'),
+
+    ## Admin URLs
+    path('Crear_Paquete/', views.create_paquete, name='crear_paquete'),
+    path('Crear_Conductor/', views.create_Conductor, name='crear_conductor')
 ]
